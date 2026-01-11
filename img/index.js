d49d2dc537235502e80b6e30a5f826054b99f25f
Karol Amorim <karoline.kalm360@gmail.com>
2026-01-11 12:31:19 -0300

    finalizando javascript e finalizando o projeto

--- img/index.js
+++ img/index.js
@@ -10,16 +10,48 @@ function verificar(){
         var valorSexo = document.getElementsByName('sexo')
         var idade = ano - Number(anoNascimento.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
 
+        //HOMEM
 
         if (valorSexo[0].checked){
            genero='homem'
+
            if(idade >= 0 && idade < 5){
-            
+            //bebe
+                img.setAttribute('src','./bebe-homem.jpg')
+           }else if(idade >=5 && idade < 15 ){
+            //criança
+                img.setAttribute('src', './crianca-masculina.avif')
+           }else if(idade >= 15 && idade < 50){
+            //jovem
+                img.setAttribute('src','./jovem-homem.avif')
+           }else if(idade >= 50){
+            //idoso
+                img.setAttribute('src','./idoso-homem.avif')
            }
+
+        //MULHER
+
         }else if(valorSexo[1].checked){
            genero='mulher'
+
+           if(idade >= 0 && idade < 5){
+            //bebe
+                img.setAttribute('src','./bebe-mulher.jpg')
+           }else if(idade >=5 && idade < 15 ){
+            //criança
+                img.setAttribute('src', './crianca-feminina.jpg')
+           }else if(idade >= 15 && idade < 50){
+            //jovem
+                img.setAttribute('src','./jovem-mulher.avif')
+           }else if(idade >= 50){
+            //idoso
+                img.setAttribute('src','./idoso-mulher.jpg')
+           }
         }
-        resultado.innerHTML=`genero: ${genero} idade: ${idade}`
+        resultado.innerHTML=`Identificamos um/a <strong>${genero}</strong> de <strong>${idade}</strong> anos.`
+        resultado.appendChild(img)
     }
 }
