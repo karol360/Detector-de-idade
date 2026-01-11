33054d1fe19fd43f104b42fe92014eb3e41952c8
Karol Amorim <karoline.kalm360@gmail.com>
2026-01-11 11:43:18 -0300

    Arrumando o Css e fazendo o Javascript

--- img/index.js
+++ img/index.js
@@ -1,5 +1,25 @@
 function verificar(){
     var data = new Date()
-    var ano = data.getFullYear()
+    var ano = data.getFullYear() //ano atual
     var anoNascimento = window.document.getElementById('blocoano')
+    var resultado = window.document.querySelector('div#resultado')
+
+    if(anoNascimento.value.length == 0 || Number(anoNascimento.value)> ano){
+        window.alert('[ERRO] Digite o ano novamente!!')
+    } else{
+        var valorSexo = document.getElementsByName('sexo')
+        var idade = ano - Number(anoNascimento.value)
+        var genero = ''
+
+
+        if (valorSexo[0].checked){
+           genero='homem'
+           if(idade >= 0 && idade < 5){
+            
+           }
+        }else if(valorSexo[1].checked){
+           genero='mulher'
+        }
+        resultado.innerHTML=`genero: ${genero} idade: ${idade}`
+    }
 }
